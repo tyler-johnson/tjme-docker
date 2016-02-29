368608d35b4f714d10abdb62d7a30a5aecd28602
Tyler Johnson <tyler@tylerjohnson.me>
2016-02-28 18:27:38 -0700

    removed x-powered-by disable

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 process.env.NODE_ENV = "production";
 
-var tjme = require("@mrgalaxy/tjme")();
-tjme.disable("x-powered-by");
-
-tjme.listen(80, function() {
+require("@mrgalaxy/tjme")().listen(80, function() {
 	var addr = this.address();
 	console.log("tylerjohnson.me listening at http://%s:%s", addr.address, addr.port);
 	console.log("Enter Ctrl-C to stop the server.");
